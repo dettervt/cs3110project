42fc17bd8e13a0f67b5c3e7031bd04399b0b5060
Ian Hoffman <ijh6@cornell.edu>
2015-12-05 17:16:17 -0500

    Mli is officially done

--- Display.mli
+++ Display.mli
@@ -2,57 +2,14 @@ open Graphics
 open Game
 
 (*
-  Draws a grid with the bottom left at x, y, square width w,
-  square spacing s, dimensions d-by-d, and fill true or false
+  Takes current mouse coordinates and outputs the position they
+  correspond to, or None if the mouse is not on a board square
 *)
-val drawgrid : int -> int -> int -> int -> int -> bool -> unit
-
-(*
-  Draws a list of strings vertically or horizontally, with the
-  bottom left of the first character at x, y, the list of strings
-  sl, text size s, and text spacing sp, with across vs. down
-  orientation true or false
-*)
-val drawtextlist : int -> int -> string list -> int -> int -> bool -> unit
-
-(*
-  Draws a peg at x, y (red or white, based on hit/miss bool)
-*)
-val draw_peg : int -> int -> bool -> unit
-
-(*
-  Draws a ship piece at x, y of type ship
-*)
-val draw_ship : int -> int -> ship -> unit
-
-(*
-  Draws the players' guesses on the top half of the board
-*)
-val draw_peg_board : board -> unit
-
-(*
-  Draws the players' ships on the bottom half of the board
-*)
-val draw_ship_board : board -> unit
-
-(*
-  Draws a blind for multiplayer, between players' turns
-*)
-val draw_blind: unit -> unit
-
-(*
-  Draws the game stats (Turn number, players' active ships), with
-  columns for players and ship names
-*)
-val draw_stats: game -> unit
-
-(*
-  Draws the console with the last 4-5 lines of console text
-*)
-val draw_console : string list -> unit
+val quantize_mouse : unit -> (position * string) option
 
 (*
   Accepts game, which gives information about what board to draw,
   current player turn, all boards, players, ship positions, pegs.
+  Also accepts a string list representing the items in the console.
 *)
-val draw_game : game -> unit
+val draw_game : game -> string list -> unit
