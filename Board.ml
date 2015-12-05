69158899ca35705ee860fcdab09ac997860ff2a5
Ian Hoffman <ijh6@cornell.edu>
2015-12-05 17:15:42 -0500

    I didn't change nothin' I swear officer

--- Board.ml
+++ Board.ml
@@ -1,23 +1,23 @@
 
-type ship_name = 
-  |Carrier |Battleship |Cruiser 
-  |Destroyer  |Patrol 
+type ship_name =
+  |Carrier |Battleship |Cruiser
+  |Destroyer  |Patrol
 
 type ship = {name : ship_name; mutable hit : bool}
 
-type square = 
+type square =
   |Ship of ship
-  |Empty 
-  |Peg of bool 
+  |Empty
+  |Peg of bool
 
 type position = (char * int)
 
 type board = (position * square ref) list
 
-type player_model = {board : board; 
+type player_model = {board : board;
   pboard : board; mutable ships : ship list}
 
-let position_list = 
+let position_list =
   let yaxis = ['A';'B';'C';'D';'E';'F';'G';'H';'I';'J'] in
   let xaxis = [10;9;8;7;6;5;4;3;2;1] in
     let rec matcher xs ys = match ys with
@@ -29,7 +29,7 @@ let position_list =
 
 let ship_cons = fun s -> {name = s; hit = false}
 
-let create_model () = let pos = position_list in 
+let create_model () = let pos = position_list in
   let shp_bd = List.fold_left (fun acc a -> acc@[(a, ref Empty)]) [] pos in
   let peg_bd = List.fold_left (fun acc a -> acc@[(a, ref Empty) ]) [] pos in
   {board = shp_bd; pboard = peg_bd ; ships = []}
@@ -37,16 +37,16 @@ let create_model () = let pos = position_list in
 let rec add_ship shp plist bd = match plist with
   |[] -> ()
   |h::t -> let s = ship_cons shp in
-    bd.ships <- bd.ships @ [s]; 
+    bd.ships <- bd.ships @ [s];
     (List.assoc h bd.board) := (Ship(s)) ; add_ship shp t bd
 
-let is_won bd = let ships = bd.ships in let rec matcher shps = 
+let is_won bd = let ships = bd.ships in let rec matcher shps =
   match shps with
     |[] -> true
     |h::t -> if h.hit = false then false else matcher t
   in matcher ships
 
-let is_sunk shp_name bd = let shps = bd.ships in 
+let is_sunk shp_name bd = let shps = bd.ships in
   List.fold_left (fun acc a -> if a.name = shp_name then
     a.hit&&acc else true&&acc) true shps
 
@@ -54,7 +54,7 @@ let check_guess pos bd = match !(List.assoc pos bd.board) with
   |Ship a -> true
   |_ -> false
 
-let update_board pos bd = let a = !(List.assoc pos bd.board) in 
+let update_board pos bd = let a = !(List.assoc pos bd.board) in
   match a with
     |Ship x -> x.hit <- true
     |_ -> ()
